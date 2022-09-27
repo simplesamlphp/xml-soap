ae4cb146c539849ef00c503b48e70a783768e245
Tim van Dijen <tvdijen@gmail.com>
2022-09-27 20:53:58 +0200

    Bump dependencies

---
+++ src/XML/env/Reason.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace SimpleSAML\SOAP\XML\env;
+
+use DOMElement;
+use SimpleSAML\Assert\Assert;
+use SimpleSAML\SOAP\XML\Text;
+use SimpleSAML\XML\Exception\InvalidDOMElementException;
+use SimpleSAML\XML\Exception\SchemaViolationException;
+
+/**
+ * Class representing a env:Reason element.
+ *
+ * @package simplesaml/xml-soap
+ */
+final class Reason extends AbstractSoapElement
+{
+    /** @var \SimpleSAML\SOAP\XML\Text[] */
+    protected array $text;
+
+
+    /**
+     * Initialize a env:Reason
+     *
+     * @param \SimpleSAML\SOAP\XML\Text[] $text
+     */
+    public function __construct(array $text)
+    {
+        $this->setText($text);
+    }
+
+
+    /**
+     * @return \SimpleSAML\SOAP\XML\Text[]
+     */
+    public function getText(): array
+    {
+        return $this->text;
+    }
+
+
+    /**
+     * @param \SimpleSAML\SOAP\XML\Text $text
+     */
+    private function setText(array $text): void
+    {
+        Assert::allIsInstanceOf($text, Text::class, SchemaViolationException::class);
+        Assert::minCount(1, $text, SchemaViolationException::class);
+        $this->text = $text;
+    }
+
+
+    /**
+     * Convert this element to XML.
+     *
+     * @param \DOMElement|null $parent The element we should append this element to.
+     * @return \DOMElement
+     */
+    public function toXML(DOMElement $parent = null): DOMElement
+    {
+        $e = $this->instantiateParentElement($parent);
+
+        foreach ($this->text as $text) {
+            $text->toXML($e);
+        }
+
+        return $e;
+    }
+
+    /**
+     * Convert XML into a Value
+     *
+     * @param \DOMElement $xml The XML element we should load
+     * @return self
+     *
+     * @throws \SimpleSAML\XML\Exception\InvalidDOMElementException
+     *   If the qualified name of the supplied element is wrong
+     */
+    public static function fromXML(DOMElement $xml): object
+    {
+        Assert::same($xml->localName, 'Text', InvalidDOMElementException::class);
+        Assert::same($xml->namespaceURI, Text::NS, InvalidDOMElementException::class);
+
+        $text = Text::getChildrenOfClass($xml);
+        Assert::minCount(1, $text, SchemaViolationException::class);
+
+        return new self($text);
+    }
+}
---
+++ src/XML/env/Text.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace SimpleSAML\SOAP\XML\env;
+
+use DOMAttr;
+use DOMElement;
+use DOMNameSpaceNode;
+use SimpleSAML\Assert\Assert;
+use SimpleSAML\XML\Exception\InvalidDOMElementException;
+use SimpleSAML\XML\XMLStringElementTrait;
+
+/**
+ * Class representing a env:Value element.
+ *
+ * @package simplesaml/xml-soap
+ */
+final class Value extends AbstractSoapElement
+{
+    use XMLStringElementTrait;
+
+    /** @var \DOMAttr|null */
+    protected $node = null;
+
+
+    /**
+     * Initialize a env:Value
+     *
+     * @param string $content
+     * @param DOMAttr|null $node
+     */
+    public function __construct(string $content, ?DOMAttr $node = null)
+    {
+        $this->setContent($content);
+        $this->setNode($node);
+    }
+
+
+    /**
+     * @return \DOMAttr|null
+     */
+    public function getNode(): ?DOMAttr
+    {
+        return $this->node;
+    }
+
+
+    /**
+     * @param \DOMAttr|null $node
+     */
+    private function setNode(?DOMAttr $node): void
+    {
+        $this->node = $node;
+    }
+
+
+    /**
+     * Validate the content of the element.
+     *
+     * @param string $content  The value to go in the XML textContent
+     * @throws \SimpleSAML\Assert\AssertionFailedException on failure
+     * @return void
+     */
+    protected function validateContent(string $content): void
+    {
+        Assert::notWhitespaceOnly($content);
+    }
+
+
+    /**
+     * Convert this element to XML.
+     *
+     * @param \DOMElement|null $parent The element we should append this element to.
+     * @return \DOMElement
+     */
+    public function toXML(DOMElement $parent = null): DOMElement
+    {
+        $e = $this->instantiateParentElement($parent);
+        $e->textContent = $this->getContent();
+
+        if ($this->node !== null) {
+            if (!($e->hasAttribute($this->node->localName))) {
+                $e->setAttributeNode($this->getNode());
+            }
+        }
+
+        return $e;
+    }
+
+    /**
+     * Convert XML into a Value
+     *
+     * @param \DOMElement $xml The XML element we should load
+     * @return self
+     *
+     * @throws \SimpleSAML\XML\Exception\InvalidDOMElementException
+     *   If the qualified name of the supplied element is wrong
+     */
+    public static function fromXML(DOMElement $xml): object
+    {
+        Assert::same($xml->localName, 'Value', InvalidDOMElementException::class);
+        Assert::same($xml->namespaceURI, Value::NS, InvalidDOMElementException::class);
+
+        @list($prefix, $localName) = preg_split('/:/', $xml->textContent, 2);
+        if ($localName === null) {
+            // We don't have a prefixed value here
+            $prefix = null;
+        }
+
+        $node = null;
+        if ($prefix !== null) {
+            $node = $xml->ownerDocument->documentElement->getAttributeNode('xmlns:' . $prefix);
+            if ($node !== false) {
+                $node = new DOMAttr('xmlns:' . $prefix, $node->namespaceURI);
+            } else {
+                $node = null;
+            }
+        }
+
+        return new self($xml->textContent, $node);
+    }
+}
